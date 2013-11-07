69b02ed71649358c47acb8eb8e02f3124a108694
Rialgar <rialgar42@gmail.com>
2013-11-08 00:50:16 +0100

    removed jquery - no autocomplete for first prototype table can be filled with data, no interface to persist it, yet filter menu and login are still mocked

--- JS/ui.js
+++ JS/ui.js
@@ -44,41 +44,112 @@ window.addEventListener("load", function(){
 		activate("stats");
 	});
 
-	var values = {
-		name: ["Miete", "DSL", "Käse", "Milch", "Brot", "Wasser"]
-	};
-
-	function edit(td, type){
-		if(!td.editing) {
-			td.editing = true;
-			var val = td.textContent;
-			td.textContent="";
-			var input = document.createElement("input");
-			input.value = val;
-			td.appendChild(input);
-			$(input).autocomplete({source:values[type]});
-			$(input).on("keypress", function(evt){
-				if((evt.keyCode || evt.which) == 13){
-					stopEditing(this);
-				}
-			});
-			input.focus();
-		}
-	}
-
-	function stopEditing(input)
+	function createNewRow()
 	{
-		var td = input.parentNode;
-		if(td.editing) {
-			td.editing = false;
-			var val = input.value;
-			td.removeChild(input);
-			td.textContent = val;
+		var tr = document.createElement("tr");
+		tr.setAttribute("id", "data_new");
+
+		var td = document.createElement("td");
+		td.setAttribute("contenteditable", "true");
+		td.setAttribute("class", "date");
+		td.addEventListener("blur", saveEdit);
+		td.addEventListener("focus", selectAll);
+		tr.appendChild(td);
+
+		var td = document.createElement("td");
+		td.setAttribute("contenteditable", "true");
+		td.setAttribute("class", "name");
+		td.addEventListener("blur", saveEdit);
+		td.addEventListener("focus", selectAll);
+		tr.appendChild(td);
+
+		var td = document.createElement("td");
+		td.setAttribute("contenteditable", "true");
+		td.setAttribute("class", "price");
+		td.addEventListener("blur", saveEdit);
+		td.addEventListener("focus", selectAll);
+		tr.appendChild(td);
+
+		var td = document.createElement("td");
+		td.setAttribute("contenteditable", "true");
+		td.setAttribute("class", "category");
+		td.addEventListener("blur", saveEdit);
+		td.addEventListener("focus", selectAll);
+		tr.appendChild(td);
+
+		document.getElementById("data").appendChild(tr);
+	}
+
+	var maxId = 0;
+	var names = [];
+	var categories = [];
+	var data = {};
+
+	function show(id){
+		var row = document.getElementById("data_"+id);
+		if(row && data[id]){
+			var tds = row.getElementsByTagName("td");
+			tds[0].textContent = data[id].date.getFullYear() + "-" + (data[id].date.getMonth()+1) + "-" + data[id].date.getDate();
+			tds[1].textContent = data[id].name >= 0 ? names[data[id].name] : "";
+			tds[2].textContent = data[id].price.toFixed(2) + "€";
+			tds[3].textContent = data[id].category >= 0 ? categories[data[id].category] : "";
 		}
 	}
 
-	$( "tr>td:nth-child(2)" ).on("click", function(){
-		edit(this, "name");
-	});
+	function saveEdit(){
+		var id = this.parentElement.getAttribute("id").substring(5);
+		var field = this.getAttribute("class");
+		var value = this.textContent;
+
+		if(id == "new"){
+			maxId++;
+			id = maxId;
+			var date = new Date();
+			data[id] = {
+				date: date,
+				name: -1,
+				price: 0,
+				category: -1
+			}
+			this.parentElement.setAttribute("id", "data_"+id);
+			createNewRow();
+		}
+
+		if(field == "date") {
+			var arr = value.split("-");
+
+			var date = new Date(0);
+			date.setFullYear(parseInt(arr[0]));
+			date.setMonth(parseInt(arr[1])-1);
+			date.setDate(parseInt(arr[2]));
+			if(!isNaN(date.valueOf())){
+				data[id].date = date;
+			}
+			console.log(date);
+		} else if(field == "name") {
+			var index = names.indexOf(value)
+			if( index < 0 && value != ""){
+				index = names.push(value)-1;
+			}
+			data[id].name = index;
+		} else if(field == "price") {
+			data[id].price = parseFloat(value);
+		} else if(field == "category") {
+			var index = categories.indexOf(value)
+			if( index < 0 && value != ""){
+				index = categories.push(value)-1;
+			}
+			data[id].category = index;
+		}
+		show(id);
+	}
+
+	function selectAll(){
+		var selection = window.getSelection();
+  		selection.selectAllChildren(this);
+	}
+
+	$("#data td").on("blur", saveEdit);
+	$("#data td").on("focus", selectAll);
 
 });
