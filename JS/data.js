1c58a1d0afa7a4b62846ddceb81def12642fee2e
Rialgar <rialgar42@gmail.com>
2013-12-21 02:05:37 +0100

    conversion to string is white enough for the time being

--- JS/data.js
+++ JS/data.js
@@ -38,7 +38,7 @@ define(function(){
 			return {
 				date: 2, //date.valueOf()/(1000*60*60*24) as binaryString
 				name: 2, //Integer id as binary String
-				price: 3, //Price times 100 as binary String
+				price: 3, //Price times 100 + 8,388,608 as binary String
 				category: 2, //Integer id as binary String
 				field: 9, //Sum of the above (fields might be added)
 			};
@@ -48,19 +48,29 @@ define(function(){
 		}
 	}
 
-	var fileVersion = 1;
-
-	function pad(number, width, padString) {
-		padString = padString || '0';
-		number = number.toString();
-		return (
-			number.length >= width ?
-				number 
-			:
-				new Array(width - number.length + 1).join(padString) + number
-		);
+	function getIndexSize(version){
+		if(version == 1)
+		{
+			return {
+				maxId: 4,
+				chunk: {
+					version: 2,
+					minDate: 2,
+					maxDate: 2,
+					minPrice: 3,
+					maxPrice: 3,
+					perName: 2,
+					perCategory: 2
+				}
+			}
+		} else {
+			alert("something is wrong, please reload");
+			throw new Error("Something is wrong, please reload");
+		}	
 	}
 
+	var fileVersion = 1;
+	
 	function numberToString(number, bits){
 		var n = Math.floor(number);
 		var out = "";
@@ -106,7 +116,7 @@ define(function(){
 		out += numberToString(name+1, getEntrySize(fileVersion).name);
 
 		var price = data.price*100;
-		out += numberToString(price, getEntrySize(fileVersion).price);
+		out += numberToString(price+8388608, getEntrySize(fileVersion).price);
 
 		var category = data.category;
 		out += numberToString(category+1, getEntrySize(fileVersion).category);
@@ -131,7 +141,7 @@ define(function(){
 		return {
 			date: date,
 			name: stringToNumber(nameString)-1,
-			price: stringToNumber(priceString)/100,
+			price: (stringToNumber(priceString)-8388608)/100,
 			category: stringToNumber(categoryString)-1
 		}
 	}
@@ -143,44 +153,40 @@ define(function(){
 		{
 			console.assert(index.version == fileVersion);
 
-			var indexMin = [
-				index.version,
-				index.maxId,
-				index.names,
-				index.categories,
-				[]
-			]
+			var str = "";
+
+			var sizes = getIndexSize(index.version);
+
+			str += numberToString(index.version,2);
+			str += numberToString(index.maxId, sizes.maxId);
+			str += JSON.stringify(index.names);
+			str += JSON.stringify(index.categories);
 
 			for (var i = 0; i < index.chunks.length; i++) {
 				var chunk = index.chunks[i];
 
-				var chunkMin = [
-					chunk.version,
-					[
-						dateToString(chunk.dateRange.min),
-						dateToString(chunk.dateRange.max)
-					],
-					[chunk.priceRange.min, chunk.priceRange.max],
-					[],
-					[]
-				]
+				str += numberToString(chunk.version, sizes.chunk.version);
+				str += dateToString(chunk.dateRange.min);
+				str += dateToString(chunk.dateRange.max);
+				str += numberToString(chunk.priceRange.min*100+8388608, sizes.chunk.minPrice);
+				str += numberToString(chunk.priceRange.max*100+8388608, sizes.chunk.maxPrice);
 
 				for(var name in chunk.names){
 					if(chunk.names.hasOwnProperty(name)){
-						chunkMin[3].push(parseInt(name));
+						str += numberToString(name+2, sizes.chunk.perName);
 					}
 				}
+
+				str += numberToString(0, sizes.chunk.perName);
 
 				for(var category in chunk.categories){
 					if(chunk.categories.hasOwnProperty(category)){
-						chunkMin[4].push(parseInt(category));
+						str += numberToString(category+2, sizes.chunk.perCategory);
 					}
 				}
 
-				indexMin[4].push(chunkMin);
+				str += numberToString(0, sizes.chunk.perCategory);
 			};
-
-			var str = JSON.stringify(indexMin);
 
 			//TODO encrypt index and send it to server, callback for response?
 			//use extra module for crypto
@@ -196,45 +202,68 @@ define(function(){
 			return;
 		}
 
-		var indexMin = JSON.parse(str);
-		index = {
-			version: indexMin[0],
-			maxId: indexMin[1],
-			names: indexMin[2],
-			categories: indexMin[3],
-			chunks: []
-		}
+		var position = 0;
 
-		if(index.version > fileVersion){
-			alert("Something is wrong, please reload");
-			throw new Error("Something is wrong, please reload");
-		}
+		index = {};
 
-		for (var i = 0; i < indexMin[4].length; i++) {
-			var chunkMin = indexMin[4][i];
-			var chunk = {
-				version: chunkMin[0],
-				dateRange: {
-					min: stringToDate(chunkMin[1][0]),
-					max: stringToDate(chunkMin[1][1])
-				},
-				priceRange: {
-					min: chunkMin[1][0],
-					max: chunkMin[1][1]
-				},
-				names: {},
-				categories: {}
-			}
-			for (var j = 0; j < chunkMin[3].length; j++) {
-				var name = chunkMin[3][j];
-				chunk.names[name] = true;
-			};
-			for (var j = 0; j < chunkMin[4].length; j++) {
-				var category = chunkMin[4][j];
-				chunk.categories[category] = true;
-			};
+		index.version = stringToNumber(str.substr(position,2));
+		position += 2;
+		var sizes = getIndexSize(index.version);
+
+		index.maxId = stringToNumber(str.substr(position,sizes.maxId));
+		position += sizes.maxId;
+		
+		var pos2 = position;
+		while (str[pos2] != "]"){
+			pos2++;
+		}
+		pos2++;
+		index.names = JSON.parse(str.substring(position,pos2));
+		position = pos2;
+
+		while (str[pos2] != "]"){
+			pos2++;
+		}
+		pos2++;
+		index.categories = JSON.parse(str.substring(position,pos2));
+		position = pos2;
+
+		index.chunks = [];
+		while(position < str.length){
+			var chunk = {};
 			index.chunks.push(chunk);
-		};
+
+			chunk.version = stringToNumber(str.substr(position, sizes.chunk.version));
+			position += sizes.chunk.version;
+
+			chunk.dateRange = {};
+			chunk.dateRange.min = stringToDate(str.substr(position, sizes.chunk.minDate));
+			position += sizes.chunk.minDate;
+			chunk.dateRange.max = stringToDate(str.substr(position, sizes.chunk.maxDate));
+			position += sizes.chunk.maxDate;
+
+			chunk.priceRange = {};
+			chunk.priceRange.min = (stringToNumber(str.substr(position, sizes.chunk.minPrice))-8388608)/100;
+			position += sizes.chunk.minPrice;
+			chunk.priceRange.max = (stringToNumber(str.substr(position, sizes.chunk.maxPrice))-8388608)/100;
+			position += sizes.chunk.maxPrice;
+
+			chunk.names = [];
+			do{
+				var name = stringToNumber(str.substr(position, sizes.chunk.perName));
+				chunk.names.push(name-2);
+				position += sizes.chunk.perName;
+			}while(name != 0)
+			chunk.names.pop();
+
+			chunk.categories = [];
+			do{
+				var category = stringToNumber(str.substr(position, sizes.chunk.perCategory));
+				chunk.categories.push(category-2);
+				position += sizes.chunk.perName;
+			}while(category != 0)
+			chunk.categories.pop();
+		};		
 	}
 
 	function storeChunk(id){
@@ -249,7 +278,7 @@ define(function(){
 		}
 
 		if(chunk.length < chunkSize){
-			str += new Array(entrySize.field+1).join(0);
+			str += new Array(entrySize.field+1).join(String.fromCharCode(0));
 
 			for(var i = chunk.length+1; i < chunkSize; i++){
 				for(var j = 0; j < entrySize.field; j++){
@@ -266,7 +295,7 @@ define(function(){
 
 	function isAllZeros(string){
 		for (var i = 0; i < string.length; i++) {
-			if(string[i] != "0"){
+			if(string.charCodeAt(i) != 0){
 				return false;
 			}
 		}
@@ -500,7 +529,8 @@ define(function(){
 	});
 	Data.debug.__defineGetter__("chunks", function(){
 		return chunks;
-	})
+	});
+	Data.debug.stringToData = stringToData;
 
 	return Data;
 
